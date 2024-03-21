2207df8b275db279266ed7f1bf7dc5206b1ee711
Andrea Massa <andreamassa1999@gmail.com>
2024-03-21 11:54:58 -0400

    Created button visual interaction

--- app/static/scripts/listen.js
+++ app/static/scripts/listen.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
     
+    // Start listening when the start btn is clicked
+    $('#start_btn').click(function (e) { 
+        $(this).attr('disabled', true);        
+        $('#stop_btn').removeAttr('disabled');
+    });
+
+    // Stop listening when the stop btn is clicked
+    $('#stop_btn').click(function (e) { 
+        $(this).attr('disabled', true);        
+        $('#start_btn').removeAttr('disabled');
+    });
 });
