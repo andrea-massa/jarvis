7a2999e939c2b73bc9940d98362f2c722b443a17
Andrea Massa <andreamassa1999@gmail.com>
2024-03-21 12:09:48 -0400

    Added listen and stop listening functionality

--- app/static/scripts/listen.js
+++ app/static/scripts/listen.js
@@ -1,14 +1,40 @@
+let audioStream;
+
+
 $(document).ready(function () {
     
     // Start listening when the start btn is clicked
     $('#start_btn').click(function (e) { 
+        startListening()
         $(this).attr('disabled', true);        
         $('#stop_btn').removeAttr('disabled');
     });
 
+
+
     // Stop listening when the stop btn is clicked
     $('#stop_btn').click(function (e) { 
+        stopListening()
         $(this).attr('disabled', true);        
         $('#start_btn').removeAttr('disabled');
     });
 });
+
+
+
+
+
+async function startListening(){
+    try {
+        audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    } catch (error) {
+        console.error('Error accessing microphone:', error);
+    }
+}
+
+async function stopListening(){
+    if (audioStream) {
+        audioStream.getTracks().forEach(track => track.stop());
+    }
+    console.log('Audio Stream')
+}
